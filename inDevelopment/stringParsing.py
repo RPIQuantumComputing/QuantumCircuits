321753df5c336790739ac82fc0a7519e2d1f7e74
dylansheils <70299835+dylansheils@users.noreply.github.com>
2022-07-23 14:51:22 -0400

    Update stringParsing.py

--- inDevelopment/stringParsing.py
+++ inDevelopment/stringParsing.py
@@ -2,6 +2,7 @@
 objectiveFunction = input("Enter objective: ")
 variableList = []
 typeList = []
+boundMap = {"n/a": [None, None]}
 stop = False
 while(not stop):
     user = input("Enter Variable (n/a to exit): ")
@@ -21,7 +22,21 @@ while(not stop):
             typeVal = user[user.find(":")+2:]
             variableList.append(name)
             typeList.append(typeVal)
-
+stop = False
+while not stop:
+    bounds = [None, None]
+    bounding = input("Enter Bounds (_ for n/a and ! for exit)")
+    if("!" in bounding):
+        stop = True
+    else:
+        variable = bounding[:bounding.find("|")-1]
+        lower = bounding[bounding.find("|")+2:bounding.find(",")]
+        if("_" not in lower):
+            bounds[1] = float(lower)
+        upper = bounding[bounding.find(",")+2:]
+        if("_" not in upper):
+            bounds[0] = float(upper)
+        boundMap[variable] = bounds
 
 maximize = True
 if("min" in objectiveFunction):
@@ -30,9 +45,14 @@ if("min" in objectiveFunction):
 objectiveFunction = objectiveFunction[4:]
 if(maximize == True):
     print("Maximizing: ", objectiveFunction)
-    for idx in range(len(typeList)):
-        print("Variable: ", variableList[idx], " Type: ", typeList[idx])
 else:
     print("Minimizing: ", objectiveFunction)
-    for idx in range(len(typeList)):
-        print("Variable: ", variableList[idx], " Type: ", typeList[idx])
+
+for idx in range(len(typeList)):
+    print("Variable: ", variableList[idx], " Type: ", typeList[idx])
+for variable in variableList:
+    if(variable in boundMap):
+        print("Variable: ", variable, " has lower bound: ", boundMap[variable][0], " and upper bound: ", boundMap[variable][1])
+        
+#sum(x[(i * 2)], _, _) <= 10
+#sum(x[(i * 2)], _, _) <= sum(y[(j * 2)], _, _)
