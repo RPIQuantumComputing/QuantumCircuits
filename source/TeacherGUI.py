1b1f47774cb726b22b93b056a498848c1ff717e7
Luxque <112588291+Luxque@users.noreply.github.com>
2022-10-25 23:47:10 -0400

    Teacher GUI
    The first implementation on the main page.

---
+++ source/TeacherGUI.py
@@ -0,0 +1,46 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.Luancher = QtWidgets.QPushButton(self.centralwidget)
+        self.Luancher.setGeometry(QtCore.QRect(270, 390, 261, 101))
+        self.Luancher.setObjectName("Luancher")
+        self.ChatBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.ChatBox.setGeometry(QtCore.QRect(720, 530, 81, 20))
+        self.ChatBox.setObjectName("ChatBox")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.Luancher.setText(_translate("MainWindow", "Launch QuantumCircuit"))
+        self.ChatBox.setText(_translate("MainWindow", "Chat Box"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
