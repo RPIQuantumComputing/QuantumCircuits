059b43db0b4a1721ffc8017624eb27c294a4735c
alittlerocket <zhangm21@rpi.edu>
2023-09-22 17:20:46 -0400

    Some updates to the Pycache, changed Linux guide.

--- installLinux.sh
+++ installLinux.sh
@@ -4,8 +4,7 @@ wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
 bash Anaconda3-4.0.0-Linux-x86_64.sh
 conda create -y -n quantumCircuits anaconda python=3.8.5
 conda activate quantumCircuits
-pip install -U --user cuda-python
-pip install -U --user cupy
-pip install -U --user pyqt5 numpy matplotlib redmail qiskit dwave-ocean-sdk strawberryfields scipy pandas 
-cd source
-python DesignerGUI.py
+pip install cuda-python
+pip install PyQt5 numpy matplotlib redmail qiskit dwave-ocean-sdk strawberryfields scipy pandas
+
+# You will need to "cd" into source yourself. Python is not very smart. Run DesignerGUI.py while in the source directory
