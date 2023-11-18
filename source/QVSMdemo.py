d9d88ab8df46b4e81a33bfd214ef07b75bfdcd4f
PeijingX <peij.xu0@gmail.com>
2023-11-17 17:15:03 -0800

    created a demo for quantum support vector machines

---
+++ source/QVSMdemo.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch
+from torch.nn.functional import relu
+
+from sklearn.svm import SVC
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import pennylane as qml
+from pennylane.templates import AngleEmbedding, StronglyEntanglingLayers
+from pennylane.operation import Tensor
+
+import matplotlib.pyplot as plt
+
+# Load the Iris dataset
+data = load_iris()
+X, y = data.data, data.target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
